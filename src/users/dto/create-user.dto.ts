3ae9c03a5a0f6cd61d080b3a5f81c54c1c3fce03
Mudipa Kishan <mudipakishanimayanga@gmail.com>
2026-04-20 07:22:48 +0530

    update

---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,22 @@
+import { IsEmail, IsEnum, IsNotEmpty, IsString, MinLength } from 'class-validator';
+
+export enum UserRole {
+  USER = 'USER',
+  ADMIN = 'ADMIN',
+}
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @MinLength(6)
+  password: string;
+
+  @IsEnum(UserRole)
+  role: UserRole;
+}
