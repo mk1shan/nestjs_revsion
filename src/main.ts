72c97d7d47689a3c5105d2172a55565627775df2
Mudipa Kishan <mudipakishanimayanga@gmail.com>
2026-04-21 22:35:46 +0530

    update prism a and secruity level

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // Security & Data Integrity
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+
+  const port = process.env.PORT || 3050;
+  await app.listen(port);
+  console.log(` Server ready at http://localhost:${port}`);
 }
 bootstrap();
--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,8 +1,17 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from 'src/generated/prisma/client';
+import { Pool } from 'pg';
+import { PrismaPg } from '@prisma/adapter-pg';
+// Import from your custom output path!
+import { PrismaClient } from '../generated/prisma/client'; 
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
+  constructor() {
+    const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+    const adapter = new PrismaPg(pool);
+
+    super({ adapter }); // This tells Prisma 7 how to talk to Postgres
+  }
 
   async onModuleInit() {
     await this.$connect();
